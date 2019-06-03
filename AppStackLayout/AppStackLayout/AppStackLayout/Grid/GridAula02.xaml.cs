8da98084012407b72037487bfd93762fa02140e2
Jeyson Gomes <jeysongustavo@gmail.com>
2019-06-03 20:50:43 -0300

    Grid via C#

--- AppStackLayout/AppStackLayout/AppStackLayout/Grid/GridAula02.xaml.cs
+++ AppStackLayout/AppStackLayout/AppStackLayout/Grid/GridAula02.xaml.cs
@@ -15,6 +15,40 @@ namespace AppStackLayout.Grid
 		public GridAula02 ()
 		{
 			InitializeComponent ();
-		}
+
+            // Criar Grid via C#
+
+            /*
+            var grid = new Xamarin.Forms.Grid
+            {
+                RowSpacing = 20,
+                ColumnSpacing = 20
+            };
+
+            var label = new Label { Text = "Label 1" };
+            grid.Children.Add(label, 0, 0);
+            Xamarin.Forms.Grid.SetRowSpan(label, 2);
+            Xamarin.Forms.Grid.SetColumnSpan(label, 2);
+            Xamarin.Forms.Grid.SetRow(label, 0);
+            Xamarin.Forms.Grid.SetColumn(label, 0);
+
+            grid.RowDefinitions.Add(new RowDefinition
+            {
+                Height = new GridLength(100, GridUnitType.Absolute)
+            });
+
+            grid.RowDefinitions.Add(new RowDefinition
+            {
+                Height = new GridLength(2, GridUnitType.Star)
+            });
+
+            grid.RowDefinitions.Add(new RowDefinition
+            {
+                Height = new GridLength(1, GridUnitType.Auto)
+            });
+
+            */
+
+        }
 	}
 }
